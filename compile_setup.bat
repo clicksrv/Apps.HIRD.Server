5b70440a1555ffb180881b42ee8dacbe843c5f03
Saurav Kumar Sahoo <srv.k.s.11@gmail.com>
2022-03-24 13:02:07 +0530

    Deploying Release v0.2.5

--- compile_setup.bat
+++ compile_setup.bat
@@ -1,20 +1,20 @@
 @echo off
 set /p v="Enter Version Name in format (vX.Y.Z[-suffix]): "
 
-set pulling_msg=[ Pulling any Unsynced Changes ]
-set publish_msg=[ Publishing Files for Setup ]
-set compile_msg=[ Compiling Setup Exe ]
-set deploy_msg=[ Deploying Release ]
+set pulling_msg=Pulling any Unsynced Changes
+set publish_msg=Publishing Files for Setup
+set compile_msg=Compiling Setup Exe
+set deploy_msg=Deploying Release %v%
 
 title Compiling HIRD Windows Server Setup
 
 echo:
-echo %pulling_msg%
+echo [ %pulling_msg% ]
 echo:
 
 cd src\HIRD
 echo:
-echo %publish_msg%
+echo [ %publish_msg% ]
 echo:
 
 dotnet publish -p:PublishProfile=FolderProfile
@@ -22,17 +22,17 @@ dotnet publish -p:PublishProfile=FolderProfile
 cd ..\..
 
 echo:
-echo %compile_msg%
+echo [ %compile_msg% ]
 echo:
 
 iscc setup_with_version.iss /DMyAppVersion=%v%
 
 echo:
-echo %deploy_msg%
+echo [ %deploy_msg% ]
 echo:
 
 git add .
-git commit -m "Deploying Release %v%"
+git commit -m "%deploy_msg%"
 git push origin %v%
 
 echo:
